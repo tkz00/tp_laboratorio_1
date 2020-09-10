e73f928c62da9669a641d6aaac67b1a4f33cca5a
Theo <theokatz@gmail.com>
2020-09-10 20:04:23 -0300

    Enunciado

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,3 +1,40 @@
+/*
+
+Theo Katz
+
+TP Calculadora
+
+Hacer una calculadora. Para ello el programa iniciará y contará con un menú de opciones:
+
+1. Ingresar 1er operando (A=x)
+
+2. Ingresar 2do operando (B=y)
+
+3. Calcular todas las operaciones
+    a) Calcular la suma (A+B)
+    b) Calcular la resta (A-B)
+    c) Calcular la division (A/B)
+    d) Calcular la multiplicacion (A*B)
+    e) Calcular el factorial (A!)
+
+4. Informar resultados
+    a) “El resultado de A+B es: r”
+    b) “El resultado de A-B es: r”
+    c) “El resultado de A/B es: r” o “No es posible dividir por cero”
+    d) “El resultado de A*B es: r”
+    e) “El factorial de A es: r1 y El factorial de B es: r2”
+
+5. Salir
+
+• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte,
+que contenga las funciones para realizar las cinco operaciones.
+• En el menú deberán aparecer los valores actuales cargados en los operandos A y B
+(donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado)
+• Deberán contemplarse los casos de error (división por cero, etc)
+• Documentar todas las funciones
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
